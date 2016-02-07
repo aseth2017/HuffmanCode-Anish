071e0d9f341366a002f843b272fe6b36e527e604
aseth2017 <aseth2017@pingry.org>
2016-02-07 11:09:16 -0500

    3rd Commit
    Almost done, encode and decode work, just need to fix a bug

--- HuffmanRunner.java
+++ HuffmanRunner.java
@@ -1,11 +1,12 @@
+import java.util.HashMap;
+import java.util.Map.Entry;
+import java.util.PriorityQueue;
 public class HuffmanRunner
 {
-	public static void main(String[] args)
+	public static void main(String [] args)
 	{
-		HuffmanNode root = new HuffmanNode("f", 3);
-		HuffmanNode root2 = new HuffmanNode("e", 11);
-		root.setLeft(root);
-		System.out.println("run");
-		System.out.println(root2.compareTo(root));
+		HuffmanTree tree = new HuffmanTree("Sally sells seashells by the seashore, the shells she sells are seashells");
+		System.out.println(tree.encoder("Sally sells seashells, the shells she sells are seashells"));
+		System.out.println(tree.decoder("011111011000100100010001010110010011001010110110100000110010011001111001000011000011010100000110010011001010000011010101100100110010011000010111010101101101000001100100110"));
 	}
 }
--- HuffmanTree.java
+++ HuffmanTree.java
@@ -1,33 +1,35 @@
 import java.util.HashMap;
+import java.util.*;
 import java.util.PriorityQueue;
 /**
  * Class that creates a HuffmanTree
- * Creates a Priority and initializes the tee itself using a Hashmap
+ * Creates a Priority and initializes the tree itself using a Hashmap
+ * With help from Will Long
  * @author Anish Seth
  */
 public class HuffmanTree
 {
 	private HuffmanNode root;
-	private String input;
 	private HashMap<String, Integer> map;
 	private PriorityQueue<HuffmanNode> queue;
 	
-	public HuffmanTree(String s)
+	public HuffmanTree(String input)
 	{
-		input = s;
 		map = new HashMap<String, Integer>();
 		queue = new PriorityQueue<HuffmanNode>();
 		for(int i = 0; i < input.length(); i++)
 		{
 			if(map.containsKey(input.substring(i,i + 1)))
-				map.put(input.substring(i,i + 1), map.get(input.substring(i,i + 1) + 1));
+				map.put(input.substring(i,i + 1), map.get(input.substring(i,i + 1)) + 1);
 			else
 				map.put(input.substring(i,i + 1), 1);
 		}
+		initQueue();
+		initTree();
 	}
 	public PriorityQueue<HuffmanNode> initQueue()
 	{
-		String[] key = map.keySet().toArray(new String[0]);
+		String[] key =  map.keySet().toArray(new String[0]);
 		for(int i = 0; i < key.length; i++)
 		{
 			queue.add(new HuffmanNode(key[i], map.get(key[i])));
@@ -45,16 +47,50 @@ public class HuffmanTree
 			queue.add(root);
 			hold.setLeft(top);
 			hold.setRight(topv2);
+
 		}
 		return root;
 	}
 	public String encoder(String s)
 	{
-		return "";
+		String encode = "";
+		for(int i = 0; i < s.length(); i++)
+		{
+			HuffmanNode hold = root;
+			while((hold.getLeft() != null) && (hold.getRight() != null))
+			{
+				if(hold.getLeft().getKey().contains(s.substring(i, i + 1)))
+				{
+					hold = hold.getLeft();
+					encode += "1";
+				}
+				else if(hold.getRight().getKey().contains(s.substring(i, i + 1)))
+				{
+					hold = hold.getRight();
+					encode += "0";
+				}
+			}
+		}
+		return encode;
 	}
 	public String decoder(String s)
 	{
-		return "";
+		String decode = "";
+		int i = 0;
+		while(i < s.length())
+		{
+			HuffmanNode hold = root;
+			while((hold.getLeft() != null) && (hold.getRight() != null))
+			{
+				if(s.substring(i, i + 1).equals("1"))
+					hold = hold.getLeft();
+				else if(s.substring(i, i + 1).equals("0"))
+					hold = hold.getRight();
+				i++;
+			}
+			decode += hold.getKey();
+		}
+		return decode;
 	}
 	public String toString()
 	{
